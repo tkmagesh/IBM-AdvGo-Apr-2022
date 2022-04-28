c334bbe3f382003f03c0247267e559f68083d1c2
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-28 17:00:04 +0530

    grpc - server streaming

--- 05-grpc/client/client.go
+++ 05-grpc/client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"grpc-demo/proto"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -18,6 +19,11 @@ func main() {
 	}
 	service := proto.NewAppServiceClient(clientConn)
 	ctx := context.Background()
+	//doRequestResponse(ctx, service)
+	doServerStreaming(ctx, service)
+}
+
+func doRequestResponse(ctx context.Context, service proto.AppServiceClient) {
 	addRequest := &proto.AddRequest{
 		X: 100,
 		Y: 200,
@@ -27,4 +33,26 @@ func main() {
 		log.Fatalln(err)
 	}
 	fmt.Println("Add Operation : Result =", addResponse.GetResult())
+}
+
+func doServerStreaming(ctx context.Context, service proto.AppServiceClient) {
+	req := &proto.PrimeRequest{
+		Start: 3,
+		End:   100,
+	}
+	clientStream, err := service.GeneratePrimes(ctx, req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for {
+		res, err := clientStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("Prime No :=%d\n", res.GetPrimeNo())
+	}
+	fmt.Println("Done")
 }
--- 05-grpc/server/server.go
+++ 05-grpc/server/server.go
@@ -6,6 +6,7 @@ import (
 	"grpc-demo/proto"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -23,6 +24,31 @@ func (asi *appServiceImpl) Add(ctx context.Context, req *proto.AddRequest) (*pro
 		Result: result,
 	}
 	return res, nil
+}
+
+func (asi *appServiceImpl) GeneratePrimes(req *proto.PrimeRequest, serverStream proto.AppService_GeneratePrimesServer) error {
+	start := req.GetStart()
+	end := req.GetEnd()
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			time.Sleep(500 * time.Millisecond)
+			fmt.Printf("Generated prime no : %d\n", no)
+			res := &proto.PrimeResponse{
+				PrimeNo: no,
+			}
+			serverStream.Send(res)
+		}
+	}
+	return nil
+}
+
+func isPrime(no int32) bool {
+	for i := int32(2); i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
